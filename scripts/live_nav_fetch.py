fa9577c19420641875df802add085b6829a317f3
Ranadeep <ranadeeplingutla@gmail.com>
2026-06-03 12:02:24 +0530

    Day 1: Data ingestion complete

---
+++ scripts/live_nav_fetch.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+
+funds = {
+    "hdfc_top100": 125497,
+    "sbi_bluechip": 119551,
+    "icici_bluechip": 120503,
+    "nippon_largecap": 118632,
+    "axis_bluechip": 119092,
+    "kotak_bluechip": 120841
+}
+
+for name, code in funds.items():
+
+    url = f"https://api.mfapi.in/mf/{code}"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    df = pd.DataFrame(data["data"])
+
+    df.to_csv(
+        f"data/raw/{name}.csv",
+        index=False
+    )
+
+    print(f"{name} saved successfully")
